9ac7a988bfd592bfc1e75be8cd18366f6232f777
Higor Pires <higorlpires@gmail.com>
2022-05-24 20:12:37 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,26 @@ import { PageHeader, Menu, Dropdown, Button, Tag, Typography, Row } from 'antd';
 
 const Header = () => {
   return (
-    <Menu className="headeritems"
-    items={[
-      {
-        key: '1',
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="">
-            Instagram
-          </a>
-        ),
-      },
-      {
-        key: '2',
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="">
-            facebook
-          </a>
-        ),
-      },
-      {
-        key: '3',
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="">
-           <img className="headericons" src={Headerlogo} alt="" />
-            
-          </a>
-        ),
-      },
-    ]}
-  />
+  <Menu className="header">
+    <div className="headeritems"> 
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href="">
+          <img className="headericons" src={Headerlogo} alt="" />  
+        </a>   
+      </div>
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href="">
+          <img className="headericons" src={Headerlogo} alt="" />  
+        </a>   
+      </div>
+    
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href="">
+          <img className="headericons" src={Headerlogo} alt="" />  
+        </a>   
+      </div>
+    </div>
+  </Menu>
 );
   
 }
