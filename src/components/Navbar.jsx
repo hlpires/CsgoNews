363eb5b2aa651ba66f331541ceec73199347801b
Higor Pires <higorlpires@gmail.com>
2022-05-25 19:56:32 -0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button,Menu,Typography, Avatar } from 'antd';
+import {Button,Menu,Typography, Avatar,Space } from 'antd';
 import {Link} from 'react-router-dom'
 import {HomeOutlined,BankOutlined, BulbOoutlined, HourglassOutlined, MenuOutlined} from '@ant-design/icons';
 import icon from '../images/cslogo.jpg'
@@ -8,8 +8,11 @@ const Navbar = () => {
   return (
     <div className="nav-container">
     <div className = "logo-container">
-      <Avatar src={icon} size={64} />  
-        <Link to="./">csgoNEWS</Link>      
+      <Avatar src={icon} size={64} shape="square"/>
+      
+        <Link to="./">
+          <h3 className = "navbarHeader">CSGO NEWS</h3>
+        </Link>      
       
       </div>
       <Menu theme="dark">
@@ -17,11 +20,11 @@ const Navbar = () => {
           <Link to="/home">Home</Link>
         </Menu.Item>
         <Menu.Item icon={<HourglassOutlined/>}>
-          <Link to="/csgobala">CsgoBala</Link>
+          <Link to="/csgobala">Hora da Bala</Link>
           </Menu.Item>
 
           <Menu.Item icon={<BankOutlined/>}>
-          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/exchanges">Melhores Odds</Link>
           </Menu.Item>
           
       </Menu>
