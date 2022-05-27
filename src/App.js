2c5b1d67ce0d2b409523f68fc27e43cedf80d2ea
Higor Pires <higorlpires@gmail.com>
2022-05-27 17:18:50 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
         <div className='main'>
           <div className='navbar'> <Navbar/> </div>         
         </div>
+        <Home/>
 
         </Layout>
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {millify} from 'millify'
+import { Typography,Row,Col,Statistic } from 'antd'
+import {Link} from 'react-router-dom'
+
+const {Title} = Typography;
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+  <>
+    <Title level = {2} className = 'heading'>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA Counter Strike Global Ofensive NEWS</Title>
+    <Row className = 'news'>
+      <Col span = {12} >< Statistic title= ' Numero de jogos ' value= '5'/> </Col>
+      <Col span = {12} >< Statistic title= ' Time vencedor ' value= '5'/> </Col>
+      <Col span = {12} >< Statistic title= ' Resultado da partida' value= '5'/> </Col> 
+      <Col span = {12} >< Statistic title= ' Imagem da pártida ' value= '5'/> </Col>
+
+    </Row>
+    </>
   )
 }
 
