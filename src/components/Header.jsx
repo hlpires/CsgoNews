bb358ea26ed970ee5d2b6fec08135f6f2ceee292
Higor Pires <higorlpires@gmail.com>
2022-05-31 17:44:15 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,24 +10,7 @@ const Header = () => {
   return (
   <Menu className="header">
     <div className="headeritems"> 
-      <div className='headertext' >       
-        <a target="_blank" rel="noopener noreferrer" href="">
-          <img id= 'shooter'className="headericons" src={Atiradorlogo} alt="" />
-        </a>
-        <h1 className="textHeader">Counter Strike</h1>
-        <h2 className="subtextHeader">Global Ofensive NEWS</h2>   
-      </div>
-      <div>
-        <a target="_blank" rel="noopener noreferrer" href="">
-          <img className="headericons" src={InstagramLogo} alt="" />  
-        </a>   
-      </div>
-    
-      <div>
-        <a target="_blank" rel="noopener noreferrer" href="">
-          <img className="headericons" src={Facebooklogo} alt="" />  
-        </a>   
-      </div>
+      
     </div>
   </Menu>
 );
