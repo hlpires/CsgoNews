6ab18958df8afd44f41c92e23c3e2cd0b6ab1b6f
Higor Pires <higorlpires@gmail.com>
2022-06-03 02:22:18 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,8 @@ const Header = () => {
   return (
   <Menu className="header">
     <div className="headeritems"> 
-    
+    <img src={Cslogo} className = "csgoimage" alt="" />
+    <h1 className= "headerText">Counter Strike News</h1>
     </div>
   </Menu>
 );
