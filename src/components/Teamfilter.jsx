f678bfd73c36758ddbf2dcf4cd30606cc1759120
Higor Pires <higorlpires@gmail.com>
2022-06-20 20:20:34 -0300

    commit

--- src/components/Teamfilter.jsx
+++ src/components/Teamfilter.jsx
@@ -4,7 +4,7 @@ const Teamfilter = () => {
 
 const lista = ['navi','pain','spirit','imperial','MIBR'];
 const [names,setNames] = useState('null');
-
+var value
 
 
 const myTeam = (val) =>{
@@ -17,11 +17,11 @@ setNames(val.target.value)
   return (
     <div className = 'teamfilter'>
         <div className = 'teamselect'>
-        <input type="text" id="myInput" onChange={myTeam} placeholder="Pesquise Times"></input>
+        <input type="text" id="myInput" onChange={myTeam} placeholder="Pesquise Times" value = {value}></input>
 
           <div>
             {lista.filter(name => name.includes(names)).map(filteredName => (
-            <li className = 'lista'>
+            <li className = 'lista' onClick ={console.log(filteredName)}>
             {filteredName}
             </li>))}
           </div>
