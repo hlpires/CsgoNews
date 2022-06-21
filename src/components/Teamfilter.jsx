113e95f0fea6d9069c6ed3f461f9c07623ece438
Higor Pires <higorlpires@gmail.com>
2022-06-21 20:09:54 -0300

    Update Teamfilter.jsx

--- src/components/Teamfilter.jsx
+++ src/components/Teamfilter.jsx
@@ -4,11 +4,12 @@ const Teamfilter = () => {
 
 const lista = ['navi','pain','spirit','imperial','MIBR'];
 const [names,setNames] = useState('null');
-var value
+const [item,setItem] = useState('');
 
 
 const myTeam = (val) =>{
 setNames(val.target.value)
+
 }
 
 
@@ -17,11 +18,11 @@ setNames(val.target.value)
   return (
     <div className = 'teamfilter'>
         <div className = 'teamselect'>
-        <input type="text" id="myInput" onChange={myTeam} placeholder="Pesquise Times" value = {value}></input>
+        <input type="text" id="myInput" onChange={myTeam} placeholder="Pesquise Times" value = {item}></input>
 
           <div>
             {lista.filter(name => name.includes(names)).map(filteredName => (
-            <li className = 'lista' onClick ={console.log(filteredName)}>
+            <li  className = 'lista' onChange={() =>setItem(this),console.log(this)}>
             {filteredName}
             </li>))}
           </div>
