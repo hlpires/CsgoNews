86b29aaac706749fe6b3cf1728764dcb32e7a6bb
Higor Pires <higorlpires@gmail.com>
2022-06-22 11:54:08 -0300

    Update Teamfilter.jsx

--- src/components/Teamfilter.jsx
+++ src/components/Teamfilter.jsx
@@ -2,28 +2,34 @@ import  React, {useState} from 'react'
 
 const Teamfilter = () => {
 
+
+// filtro de times array com multiplos times 
 const lista = ['navi','pain','spirit','imperial','MIBR'];
 const [names,setNames] = useState('null');
-const [item,setItem] = useState('');
-
-
+const [time,setTime] = useState('null');
 const myTeam = (val) =>{
 setNames(val.target.value)
 
 }
 
+function timeescolido (elemento){
+console.log(elemento.currentTarget.textContent)
+setTime(elemento.currentTarget.textContent)
+}
 
 
 
   return (
     <div className = 'teamfilter'>
         <div className = 'teamselect'>
-        <input type="text" id="myInput" onChange={myTeam} placeholder="Pesquise Times" value = {item}></input>
+        <input type="text" id="myInput" onChange={myTeam} placeholder="Pesquise Times" value={time}></input>
 
           <div>
             {lista.filter(name => name.includes(names)).map(filteredName => (
-            <li  className = 'lista' onChange={() =>setItem(this),console.log(this)}>
+            <li  className = 'lista' onClick={timeescolido} >
             {filteredName}
+            
+            
             </li>))}
           </div>
         
