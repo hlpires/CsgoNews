b6c8aa874eadfa63838918640a000be8f116a5bf
Higor Pires <higorlpires@gmail.com>
2022-07-11 21:50:00 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,27 +7,8 @@ import './App.css';
 const App = () => {
   return (
     <div className="app">
-      <Layout>
-         <Header/>
-         <Layout>
-           <Teamfilter/>
-           <Home/>
-         </Layout>  
-        
-        <div className='footer'>
-          <Typography level = {5} style= {{color:"white",textAlign:"center"}}>
-          CsgoBala <br/>
-          All rights reserved
-          </Typography>
-          <Space>
-            
-            <Link to= "/home">Home</Link>
-            <Link to= "/csgobala">csgoBala</Link>
-            <Link to= "/exchanges">Exchanges</Link>
-          </Space>        
-        </div>
-
-      </Layout>
+      <Header/>      
+      <Home/>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {millify} from 'millify'
-import { Typography,Row,Col,Statistic,Space} from 'antd'
+
 
 import { useGetNewsQuery } from '../services/csnewsApi'
 
-const {Title} = Typography;
+
 
 var numerodejogos; 
 
@@ -32,14 +32,7 @@ const Home = () => {
   return (
   <>
   
-    <Title level = {3} className = 'heading'></Title>
 
-    <Row className = 'news'>
-      
-        <Col span = {6} >< Statistic title= ' Data do jogo ' value= {datajogo[0]}/> </Col>
-        <Col span = {6} >< Statistic title= ' Time vencedor ' value= {vencedor[0]}/> </Col>
-        
-    </Row>
 
     <div className = 'newsformat'>
     
@@ -49,54 +42,8 @@ const Home = () => {
         <img className ="imageteam" src={imagem2[0]} alt="" />   
       </div>
       
-      <Row className = 'news'>
       
-        <Col span = {6} >< Statistic title= ' Data do jogo  ' value= {datajogo[1]}/> </Col>
-        <Col span = {6} >< Statistic title= ' Time vencedor ' value= {vencedor[1]}/> </Col>
-        
-    </Row>
-
-    <div className = 'newsformat'>
     
-        <img  className ="imageteam"  src={imagem[1]} alt="" />
-
-        <div span = {3} ><h1 className = 'versus' > {vitoria[1] + ' x ' + perdedor[1]} </h1> </div>
-        <img  className ="imageteam"  src={imagem2[1]} alt="" />
-      </div>
-
-      <Row className = 'news'>
-      
-        <Col span = {6} >< Statistic title= ' Data do jogo  ' value= {datajogo[2]}/> </Col>
-        <Col span = {6} >< Statistic title= ' Time vencedor ' value= {vencedor[2]}/> </Col>
-        
-    </Row>
-
-    <div className = 'newsformat'>
-    
-        <img  className ="imageteam"  src={imagem[2]} alt="" />
-
-        <div span = {3} ><h1 className = 'versus' > {vitoria[1] + ' x ' + perdedor[2]} </h1> </div>
-        <img className ="imageteam" src={imagem2[2]} alt="" />   
-      </div>
-
-      <Row className = 'news'>
-      
-        <Col span = {6} >< Statistic title= ' Data do jogo  ' value= {datajogo[3]}/> </Col>
-        <Col span = {6} >< Statistic title= ' Time vencedor ' value= {vencedor[3]}/> </Col>
-        
-    </Row>
-
-    <div className = 'newsformat'>
-    
-        <img  className ="imageteam"  src={imagem[3]} alt="" />
-
-        <div span = {3} ><h1 className = 'versus' > {vitoria[3] + ' x ' + perdedor[3]} </h1> </div>
-        <img className ="imageteam" src={imagem2[3]} alt="" />   
-      </div>
-      
-      
-      
-   
     </>
   )
 }
